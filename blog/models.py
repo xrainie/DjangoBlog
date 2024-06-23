e9ed414d9229dbb5b373e3bdefd373d6fee52248
xrainie <xoxrainie@gmail.com>
2024-06-24 02:33:27 +0300

    add Post to admin model & add PublishManager for Post model

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,41 @@
 from django.db import models
-from time import timezone
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class PublishManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = "DR", "Черновик"
+        PUBLISHED = "PB", "Опубликовано"
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    objects = models.Manager()
+    published = PublishManager()
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
 
     def __str__(self) -> str:
         return self.title
